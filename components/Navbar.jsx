dc0ea6c82d63ac76379135e9bd73de7a5aef0c5e
Gary Dineen <dineen77@hotmail.com>
2023-08-15 16:28:34 +0200

    adding icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
-
+import{AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
-  return (
+	return (
 		<div className="fixed w-full h-20 shadow-xl z-[100]">
 			<div className="flex justify-between w-full h-full px-2 2xl:px-16">
-				<Image src="/../public/assets/navLogo.png" alt="/" width="126" height="50" />
+				<Image
+					src="/../public/assets/navLogo.png"
+					alt="/"
+					width="126"
+					height="50"
+				/>
+				<div>
+					<ul className="hidden md:flex">
+						<Link href="/">
+							<li className="ml-10 test-sm uppercase hover:border-b">Home</li>
+						</Link>
+						<Link href="/">
+							<li className="ml-10 test-sm uppercase hover:border-b">About</li>
+						</Link>
+						<Link href="/">
+							<li className="ml-10 test-sm uppercase hover:border-b">Skills</li>
+						</Link>
+						<Link href="/">
+							<li className="ml-10 test-sm uppercase hover:border-b">
+								Project
+							</li>
+						</Link>
+						<Link href="/">
+							<li className="ml-10 test-sm uppercase hover:border-b">Contact</li>
+						</Link>
+					</ul>
+					<div className="md:hidden">
+						<AiOutlineMenu size={25}/>
+					</div>
+				</div>
+			</div>
+
+			<div className=" fixed left-0 top-0 w-full h-screen bg-black/70">
+<div className=" fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3]">
+
+</div>
 			</div>
 		</div>
 	);
-}
+};
 
-export default Navbar
+export default Navbar;
