db15d1da69e24d8101b9fe79be60fb493acce4ca
Gary Dineen <dineen77@hotmail.com>
2023-08-16 19:15:20 +0200

    manin code added

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import {  AiOutlineMail} from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Main = () => {
 	return (
@@ -17,11 +21,25 @@ const Main = () => {
 						Full Stack Developer&UX&UI Designer
 					</h1>
 					<p className="py-4 text-gray-600 max-w-[70%] m-auto">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum exercitationem, labore amet quo laudantium minus corrupti reprehenderit explicabo eum. Voluptates, ex nihil pariatur rerum corporis quam ab dolorum laudantium id.
-          </p>
-          <div className="flex item-center justify-between max-w-">
-
-          </div>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+						exercitationem, labore amet quo laudantium minus corrupti
+						reprehenderit explicabo eum. Voluptates, ex nihil pariatur rerum
+						corporis quam ab dolorum laudantium id.
+					</p>
+					<div className="flex item-center justify-between max-w- py-4">
+						<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor pointer  hover:scale-110 ease-in duration-300">
+							<FaLinkedin />
+						</div>
+						<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor pointer  hover:scale-110 ease-in duration-300">
+							<FaGithub />
+						</div>
+						<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor pointer  hover:scale-110 ease-in duration-300">
+							<AiOutlineMail />
+						</div>
+						<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor pointer  hover:scale-110 ease-in duration-300">
+							<BsFillPersonLinesFill />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
