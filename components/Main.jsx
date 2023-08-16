b247e315bb9cb35ae1639eb0099b503bdb071d13
Gary Dineen <dineen77@hotmail.com>
2023-08-16 18:48:56 +0200

    nav code adding and main code

---
+++ components/Main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Main = () => {
+	return (
+		<div className="w-full h-screen text-center">
+			<div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+				<div>
+					<p className=" uppercase text-sm tracking-widest text-gray-600">
+						{" "}
+						The people who are crazy enough to think they can change the world
+						are the ones who do{" "}
+					</p>
+					<h1 className="py-4 text-gray-700">
+						Hello, Im <span className="text-[#5651e5]">Gary</span>
+					</h1>
+					<h1 className="py-2 text-gray-700">
+						Full Stack Developer&UX&UI Designer
+					</h1>
+					<p className="py-4 text-gray-600 max-w-[70%] m-auto">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum exercitationem, labore amet quo laudantium minus corrupti reprehenderit explicabo eum. Voluptates, ex nihil pariatur rerum corporis quam ab dolorum laudantium id.
+          </p>
+          <div className="flex item-center justify-between max-w-">
+
+          </div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Main;
