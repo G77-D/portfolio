23ad700e2865f9906ce044e86bb9d19fa1c8351e
Gary Dineen <dineen77@hotmail.com>
2023-08-18 19:28:05 +0200

    grid code

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,24 +1,33 @@
 
-import Image from "next/image";
-import React from "react";
-import Html from "../public/assets/skills/html.png";
-import Css from "../public/assets/skills/css.png";
-import Javascript from "../public/assets/skills/javascript.png";
-import ReactImg from "../public/assets/skills/react.png";
-import Tailwind from "../public/assets/skills/tailwind.png";
-import Github from "../public/assets/skills/github1.png";
-import Firebase from "../public/assets/skills/firebase.png";
-import NextJS from "../public/assets/skills/nextjs.png";
 
 
 import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
+		<div className=" w-full lg:h-screen p-2">
+			<div className="max-w-[1240px] mx-auto  flex flex-col justify-center h-full ">
+				<p className='text-xl tracking-widest uppercase text-[#5651e5]'>Tech used</p>
+				<h2 className='py-4'>what can do</h2>
+				{/* dev grid*/}
+				<div className=" grid md:grid-cols-2 lg:-cols-4 gap-8">
+
+<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+<div className="grid grid-cols-2 gap-4 justify-center items-center">
+
+	</div>	
+</div>
+
+
+
+        </div>
+				{/* Ux grid*/}
+        <div></div>
+			</div>
+		</div>
+	);
 }
 
-export default Skills
+
 
 export default Skills
