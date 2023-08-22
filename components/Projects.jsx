c6a49a9bca6a5441398c9ec884e87ebe8df1a04d
Gary Dineen <dineen77@hotmail.com>
2023-08-22 12:25:55 +0200

    code updates

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import propertyImg from "../public/assets/projects/property.jpg";
 import Link from "next/link";
+import ProjectItems from "./ProjectItems";
 
 const Projects = () => {
 	return (
@@ -12,26 +13,7 @@ const Projects = () => {
 				</p>
 				<h2 className="py-4">What I have built</h2>
 				<div className=" grid md:grid-cols-2 gap-8">
-
-					<div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-						<Image
-							className="rounded-xl group-hover:opacity-10"
-							src={propertyImg}
-							alt="/"
-						/>
-						<div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50] translate-y-[-50%]">
-							<h3 className="text-2xl text-white tracking-wider text-clear">
-								Project Name
-							</h3>
-							<p className="pb-4 pt-2 text-white text-center ">React JS</p>
-							<Link href="/">
-								<p className=" te4xt-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor">
-									More info
-								</p>
-							</Link>
-						</div>
-
-					</div>
+					<ProjectItems title=" Project-Tile" backgroundImg= {propertyImg}/>
 				</div>
 			</div>
 		</div>
